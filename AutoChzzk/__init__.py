39656c06faa0ef4e60aa065811bc73f2a1a0698b
wlrdb1222-ui <wlrdb1222@gmail.com>
2026-09-25 13:28:52 +0900

    Update __init__.py

--- AutoChzzk/__init__.py
+++ AutoChzzk/__init__.py
@@ -3,11 +3,12 @@ AutoChzzk 패키지 초기화.
 주요 파이프라인 함수를 패키지 최상위에서 바로 import할 수 있게 재노출한다.
 
 사용 예:
-    from AutoChzzk import audio_down, transcribe_audio, analyze_subtitle
+    from AutoChzzk import audio_down, transcribe_audio, collect_chat_data, analyze_subtitle
 """
 
 from .downloader import download, audio_down
 from .transcriber import transcribe_audio
+from .chat_collector import collect_chat_data
 from .llm_analyzer import analyze_subtitle
 from .utils import json_to_csv, count_file_tokens
 from .llm_client import get_llm_client
@@ -16,6 +17,7 @@ __all__ = [
     "download",
     "audio_down",
     "transcribe_audio",
+    "collect_chat_data",
     "analyze_subtitle",
     "json_to_csv",
     "count_file_tokens",
